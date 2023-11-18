d77df919385d6a2e18b35a462f0b2fb4b64e1a20
Michael <michaeldsl.997@gmail.com>
2023-11-18 17:30:40 -0300

    teste

--- controller/usuario.controller.js
+++ controller/usuario.controller.js
@@ -26,26 +26,140 @@ const usuarios = [
     }
 ]
 
+// find
 const find = (req,res) => {
     const id = req.params.id;
-    res.send(usuarios[id]);
+    let found = false;
+
+    usuarios.map( function(valor){
+        if(valor.id == id){
+            found = true;
+            return res.send(valor);
+        }
+    });
+
+    if(!found){
+        res.status(404).send({messege:"Não foi encontrado!"});
+    }
+
 }
 
+
+// findAll
 const findAllUsuarios = (req,res) => {
     res.send(usuarios);
 }
 
+
+// create
 const createUsuario = (req,res) => {
     const usuario = req.body;
-    if(req.body.id == null){
-        return res.send({messege:"Id não está preenchido"});
+    
+    if(Object.keys(usuario).length === 0){
+        return res.status(400).send({messege:"O corpo da mensagem está vazio!"});
     }
+
+// Validação de atributos
+    if(!usuario.id){
+        return res.status(400).send({messege:"O campo 'id' não foi encontrado!"});
+    }
+
+    if(!usuario.nome){
+        return res.status(400).send({messege:"O campo 'nome' não foi encontrado!"});
+    }
+
+    if(!usuario.sobrenome){
+        return res.status(400).send({messege:"O campo 'sobrenome' não foi encontrado!"});
+    }
+
+    if(!usuario.idade){
+        return res.status(400).send({messege:"O campo 'idade' não foi encontrado!"});
+    }
+
+    if(!usuario.email){
+        return res.status(400).send({messege:"O campo 'email' não foi encontrado!"});
+    }
+
+    if(!usuario.senha){
+        return res.status(400).send({messege:"O campo 'senha' não foi encontrado!"});
+    }
+
+
     usuarios.push(usuario);
-    res.send(usuarios);
+    res.status(201).send(usuarios);
 }
+
+// update
+const updateUsuario = (req,res) => {
+    const id = req.params.id;
+    const usuario = req.body;
+    let found = false;
+
+    if(Object.keys(usuario).length === 0){
+        return res.status(400).send({messege:"O corpo da mensagem está vazio!"});
+    }
+
+// Validação de atributos
+    if(!usuario.id){
+        return res.status(400).send({messege:"O campo 'id' não foi encontrado!"});
+    }
+
+    if(!usuario.nome){
+        return res.status(400).send({messege:"O campo 'nome' não foi encontrado!"});
+    }
+
+    if(!usuario.sobrenome){
+        return res.status(400).send({messege:"O campo 'sobrenome' não foi encontrado!"});
+    }
+
+    if(!usuario.idade){
+        return res.status(400).send({messege:"O campo 'idade' não foi encontrado!"});
+    }
+
+    if(!usuario.email){
+        return res.status(400).send({messege:"O campo 'email' não foi encontrado!"});
+    }
+
+    if(!usuario.senha){
+        return res.status(400).send({messege:"O campo 'senha' não foi encontrado!"});
+    }
+
+    usuarios.map( function(valor, index){
+        if(valor.id == id){
+            found = true;
+            usuarios[index] = usuario;
+            return res.send(usuarios[index]);
+        }
+    });
+
+    if(!found){
+        res.status(404).send({messege:"Não foi encontrado!"});
+    }
+}
+
+// delete
+const deleteUsuario = (req,res) => {
+    const id = req.params.id;
+    let found = false;
+
+    usuarios.map( function(valor, index){
+        if(valor.id == id){
+            found = true;
+            usuarios.splice(index,1);
+            return res.send(valor);
+        }
+    });
+
+    if(!found){
+        res.status(404).send({messege:"Não foi encontrado!"});
+    }
+}
+
 
 module.exports = {
     find,
     findAllUsuarios,
-    createUsuario
+    createUsuario,
+    updateUsuario,
+    deleteUsuario
 }
--- router/usuario.router.js
+++ router/usuario.router.js
@@ -5,4 +5,7 @@ router.get("/find/:id", usuario.find);
 router.get("/findAll", usuario.findAllUsuarios);
 router.post("/create", usuario.createUsuario);
 
+router.put("/update/:id", usuario.updateUsuario);
+router.delete("/delete/:id", usuario.deleteUsuario);
+
 module.exports = router;
